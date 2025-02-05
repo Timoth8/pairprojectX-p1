867c53e11c9ddb6f64349bdd70e8e5f38c07e5d2
timothy <timothyhengki23@gmail.com>
2025-02-05 22:18:28 +0700

    migration

---
+++ helper/bcyrpt.js
@@ -0,0 +1,12 @@
+const bcrypt = require('bcryptjs')
+
+const hashPassword = (password) => {
+    const salt = bcrypt.genSaltSync(10)
+    return bcrypt.hashSync(password, salt)
+}
+
+const comparePassoword = (password, hashedPassword) => {
+    return bcrypt.compareSync(password, hashedPassword)
+}
+
+module.exports = {hashPassword, comparePassoword};
---
+++ migrations/20250205133158-create-profile.js
@@ -0,0 +1,43 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Profiles', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      gender: {
+        type: Sequelize.STRING
+      },
+      bio: {
+        type: Sequelize.STRING
+      },
+      profilePicture: {
+        type: Sequelize.STRING
+      },
+      phoneNumber: {
+        type: Sequelize.STRING
+      },
+      bornDate: {
+        type: Sequelize.DATE
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Profiles');
+  }
+};
