93bf718ec38530fdd4b732ac20f8e7a94a53b4cf
restupuspameirina <restupuspa@gmail.com>
2025-02-06 15:34:04 +0700

    json

--- Controller/controller.js
+++ Controller/controller.js
@@ -3,7 +3,7 @@ const {comment, post, profile, tag, user} = require('../models/index')
 class Controller {
     static async showHomepage (req,res){
         try {
-            
+            res.render('Homepage.ejs')
         } catch (error) {
             res.send(error)
         }
--- router.js
+++ router.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
+const Controller = require('./Controller/controller')
 
 
 // define the home page route
-
+router.use('/', Controller.showHomepage)
 
 module.exports = router
