a35d287dcfc7db4480310274fd79a33fc38a95d0
timothy <timothyhengki23@gmail.com>
2025-02-06 15:08:28 +0700

    post tampil

--- Controller/controller.js
+++ Controller/controller.js
@@ -1,9 +1,17 @@
-const {comment, post, profile, tag, user} = require('../models/index')
+const {Comment, Post, Profile, Tag, User} = require('../models/index')
 
 class Controller {
     static async showHomepage (req,res){
         try {
-            
+            res.render('Homepage.ejs')
+        } catch (error) {
+            res.send(error)
+        }
+    }
+    static async showPosts (req,res){
+        try {
+            let posts = await Post.findAll()
+            res.render('showPost.ejs', {posts})
         } catch (error) {
             res.send(error)
         }
--- router.js
+++ router.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const Controller = require('./Controller/controller')
 
 
 // define the home page route
-
+router.get('/', Controller.showHomepage)
+router.get('/posts', Controller.showPosts)
 
 module.exports = router
